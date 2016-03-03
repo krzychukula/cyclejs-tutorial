4e9d75bf9c67ca8e4c28ea7f293248fe8df57b9e
krzychukula <krzychukula@gmail.com>
2016-03-03 07:18:00 +0100

    add jspm - run the code from 1/21

--- src/init.js
+++ src/init.js
@@ -1,14 +1,10 @@
-// import Rx from 'rxjs/Rx';
-//
-// Rx.Observable.of(1,2,3)
 
-import {Observable} from 'rxjs/Observable'
-// import {map} from 'rxjs/operator/map';
-// Observable.of(1,2,3)::map(x => x + '!!!'); // etc
-import 'rxjs/add/operator/map'
+import Rx from 'rx'
 
-Observable.timer(0, 1000)
+// Logic (functional)
+Rx.Observable.timer(0, 1000)
   .map(i => `Seconds elapses ${i}`)
+// Effects (imperative)
   .subscribe(text => {
     const container = document.querySelector('#app')
     container.textContent = text
