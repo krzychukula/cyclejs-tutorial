d277b7b308b42c311b3133ff7acc1c29b12aa5c9
krzychukula <krzychukula@gmail.com>
2016-03-04 08:05:31 +0100

    main function and effects functions 2/21

--- src/init.js
+++ src/init.js
@@ -2,10 +2,23 @@
 import Rx from 'rx'
 
 // Logic (functional)
-Rx.Observable.timer(0, 1000)
-  .map(i => `Seconds elapses ${i}`)
+function main () {
+  return Rx.Observable.timer(0, 1000)
+    .map(i => `Seconds elapses ${i}`)
+}
+
 // Effects (imperative)
-  .subscribe(text => {
+function DOMEffects (text$) {
+  text$.subscribe(text => {
     const container = document.querySelector('#app')
     container.textContent = text
   })
+}
+
+function consoleLogEffect (msg$) {
+  msg$.subscribe(msg => console.log(msg))
+}
+
+const sink = main()
+DOMEffects(sink)
+consoleLogEffect(sink)
