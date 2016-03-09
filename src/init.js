e3406b04802ee0674f156ebfae74daab4ee039ca
krzychukula <krzychukula@gmail.com>
2016-03-09 10:52:45 +0100

    toy dom driver 7/21

--- src/init.js
+++ src/init.js
@@ -10,7 +10,19 @@ function main (sources) {
       .startWith(null)
       .flatMapLatest(() =>
         Rx.Observable.timer(0, 1000)
-            .map(i => `Seconds elapses ${i}`)
+            .map(i => {
+              return {
+                tagName: 'H1',
+                children: [
+                  {
+                    tagName: 'SPAN',
+                    children: [
+                      `Seconds elapses ${i}`
+                    ]
+                  }
+                ]
+              }
+            })
       ),
     Log: Rx.Observable.timer(0, 2000).map(i => 2 * i)
   }
@@ -19,10 +31,25 @@ function main (sources) {
 // sink: output (write) effects
 
 // drivers (imperative)
-function DOMDriver (text$) {
-  text$.subscribe(text => {
+function DOMDriver (obj$) {
+  function createElement (obj) {
+    const element = document.createElement(obj.tagName)
+    obj.children
+      .filter(c => typeof c === 'object')
+      .map(createElement)
+      .forEach(c => element.appendChild(c))
+
+    obj.children
+      .filter(c => typeof c === 'string')
+      .forEach(c => element.innerHTML += c)
+
+    return element
+  }
+  obj$.subscribe(obj => {
     const container = document.querySelector('#app')
-    container.textContent = text
+    container.innerHTML = ''
+    const element = createElement(obj)
+    container.appendChild(element)
   })
   const DOMSource = Rx.Observable.fromEvent(document, 'click')
   return DOMSource
