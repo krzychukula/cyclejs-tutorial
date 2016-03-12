84036a4319a3ae5c367815d9f36878476f21cd87
krzychukula <krzychukula@gmail.com>
2016-03-12 09:45:40 +0100

    improve toy driver a bit 10/21

--- src/init.js
+++ src/init.js
@@ -37,57 +37,44 @@ function main (sources) {
 // source: input (read) effects
 // sink: output (write) effects
 
-// drivers (imperative)
-function DOMDriver (obj$) {
-  function createElement (obj) {
-    const element = document.createElement(obj.tagName)
-    obj.children
-      .filter(c => typeof c === 'object')
-      .map(createElement)
-      .forEach(c => element.appendChild(c))
+function makeDOMDriver (mountSelector) {
+  // drivers (imperative)
+  return function DOMDriver (obj$) {
+    function createElement (obj) {
+      const element = document.createElement(obj.tagName)
+      obj.children
+        .filter(c => typeof c === 'object')
+        .map(createElement)
+        .forEach(c => element.appendChild(c))
 
-    obj.children
-      .filter(c => typeof c === 'string')
-      .forEach(c => element.innerHTML += c)
+      obj.children
+        .filter(c => typeof c === 'string')
+        .forEach(c => element.innerHTML += c)
 
-    return element
-  }
-  obj$.subscribe(obj => {
-    const container = document.querySelector('#app')
-    container.innerHTML = ''
-    const element = createElement(obj)
-    container.appendChild(element)
-  })
-  const DOMSource = {
-    selectEvents: function (tagName, eventType) {
-      return Rx.Observable.fromEvent(document, eventType)
-        .filter(ev => ev.target.tagName === tagName.toUpperCase())
+      return element
     }
+    obj$.subscribe(obj => {
+      const container = document.querySelector(mountSelector)
+      container.innerHTML = ''
+      const element = createElement(obj)
+      container.appendChild(element)
+    })
+    const DOMSource = {
+      selectEvents: function (tagName, eventType) {
+        return Rx.Observable.fromEvent(document, eventType)
+          .filter(ev => ev.target.tagName === tagName.toUpperCase())
+      }
+    }
+    return DOMSource
   }
-  return DOMSource
 }
 
 function consoleLogDriver (msg$) {
   msg$.subscribe(msg => console.log(msg))
 }
 
-// function run (mainFn, drivers) {
-//   const proxySources = {}
-//   Object.keys(drivers).forEach(key => {
-//     proxySources[key] = new Rx.Subject()
-//   })
-//   const sinks = mainFn(proxySources)
-
-//   Object.keys(drivers).forEach(key => {
-//     const source = drivers[key](sinks[key])
-//     source.subscribe(x => {
-//       proxySources[key].onNext(x)
-//     })
-//   })
-// }
-
 const drivers = {
-  DOM: DOMDriver,
+  DOM: makeDOMDriver('#app'),
   Log: consoleLogDriver
 }
 
